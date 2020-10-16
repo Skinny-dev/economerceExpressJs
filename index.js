f3e6d2dc8d290b17fd33704bdd04fc773b4ddc42
andres armas <andresarmas97@hotmail.com>
2020-10-16 05:24:24 -0500

    add to template engine properti

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const productRouter = require('./router/products');
 const app = express();
+
+app.use("/static", express.static(path.join(__dirname,"public")));
 app.set("views", path.join(__dirname, "views"));
 //establecer pug 
 app.set("view engine", "pug");
